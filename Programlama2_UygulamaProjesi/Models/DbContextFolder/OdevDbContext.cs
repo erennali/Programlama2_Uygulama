2c99df8d852b18434b70b0a1f73e4c8cd0c0d88e
Eren Ali Koca <eren_ali_koca@hotmail.com>
2024-03-20 11:43:54 +0300

    Db connection made

---
+++ Programlama2_UygulamaProjesi/Models/DbContextFolder/OdevDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DbContextFolder;
+
+public class OdevDbContext : DbContext
+{
+    public DbSet<KonuClass> Subjects { get; set; }
+    public DbSet<SoruClass> Questions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Database=ProgramlamaOdevi;User Id=erennali;Password=123456;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+}
---
+++ Programlama2_UygulamaProjesi/Models/Entities/SoruClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities;
+
+public class SoruClass
+{
+    public Guid Id { get; set; }
+    public string SoruKoku { get; set; }
+    public int KonuId { get; set; }
+    public enum ZorlukDerecesi
+    {
+        Kolay=1,
+        Orta=2,
+        Zor=3
+    }
+}
